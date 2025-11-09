5fce9dccdf9509b449e74958d71a8314fc77b9b0
JeongHwanjun <synth0003@gmail.com>
2025-11-10 01:24:58 +0900

    Customer Orderbubble 수정

--- Assets/Scripts/Cake/CakeManager.cs
+++ Assets/Scripts/Cake/CakeManager.cs
@@ -48,7 +48,8 @@ public class CakeManager : MonoBehaviour
       texture.LoadImage(imageByte);
       Sprite cakeSprite = Sprite.Create(texture,
         new Rect(0, 0, texture.width, texture.height),
-        new Vector2(0.5f, 0.5f));
+        new Vector2(0.5f, 0.5f),
+        100);
 
       Cake newCake = new Cake
       {
--- Assets/Scripts/Opening/Sales/Customer/CustomerUIHandler.cs
+++ Assets/Scripts/Opening/Sales/Customer/CustomerUIHandler.cs
@@ -4,15 +4,12 @@ using UnityEngine.UI;
 
 // 고객이 원하는 상품을 표기
 public class CustomerUIHandler : MonoBehaviour{
-    public GameObject orderImageObject;
+    public Image orderImage;
     public TextMeshProUGUI orderQuantity;
-    private SpriteRenderer spriteRenderer;
 
     public void Initialize(Cake _cake, int _orderQuantity){
-        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-
         // 케이크 이미지, 수량 할당
-        orderImageObject.GetComponent<SpriteRenderer>().sprite = _cake.sprite;
+        orderImage.sprite = _cake.sprite;
         orderQuantity.text = _orderQuantity.ToString();
     }
 
@@ -23,7 +20,5 @@ public class CustomerUIHandler : MonoBehaviour{
         } else {
             bubbleColor = new Color(0.5f, 0.5f, 0.5f, 1.0f);
         }
-
-        spriteRenderer.color = bubbleColor;
     }
 }
